2db86c6d679b3f30c2406548d61befae7266f670
SQCodes <paulsqmagadi@gmail.com>
2024-08-04 01:19:17 +0300

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 
 //  MOBILE NAV TOGGLE
 
-const navbar = document.querySelector(".mobile-nav");
-const navToggler = document.querySelector(".nav-toggle-button");
+const navbar = document.querySelector('.mobile-nav');
+const navToggler = document.querySelector('.nav-toggle-button');
+const navLinks = document.querySelectorAll('.mobile-nav a');
 
 navToggler.addEventListener('click', () => {
   navbar.classList.toggle('active');
@@ -11,6 +12,17 @@ navToggler.addEventListener('click', () => {
   } else {
     navToggler.innerHTML = '<i class="fa-solid fa-bars fa-lg"></i>';
   }
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function() {
+      navbar.classList.toggle('active');
+      if (navbar.classList.contains('active')) {
+        navToggler.innerHTML = '<i class="fa-solid fa-x fa-lg"></i>';
+      } else {
+        navToggler.innerHTML = '<i class="fa-solid fa-bars fa-lg"></i>';
+      }
+  });
 });
 
 
